bd0db10126eb5e55a1830d0327d537a6ab44a5db
mohd497 <likesteel@gmail.com>
2019-01-27 04:21:36 -0800

    write test and change controller to pass initial test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,11 @@ class UsersController < ApplicationController
 		#read users json object gave as post
 		users = params[:users]
 		first_names = []
-		user.each do |user| #iterate through it
+		users.each do |user| #iterate through it
 			first_names << user['first_name']
 		end
 
-		users = User.where(first_names)
+		users = User.where(first_name: first_names)
 		users = users.map {|user| user.first_name} #process in db
 		result = users.each_with_object(Hash.new(0)) { |users,counts| counts[a] += 1 }.map{|k,v| k if v >= 2}.compact 
 		#take out users count per first name if count 2 or above
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
+	describe "sending user data" do
+		it "should send back the results" do
+      @users = [{ id: '2', first_name: 'firstname', last_name: 'last_name' }]
+      post :user_count, params: { users: @users }
+      expect(response.status).to eq(200)
+end
+	end	
+
 end
