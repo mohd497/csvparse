0891e5488b456d90d41cb80c0f69bb87c61d52c0
mohd497 <likesteel@gmail.com>
2019-01-31 18:23:46 -0800

    routes and spec refactor

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,12 +2,10 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-	describe "sending user data" do
-		it "should send back the results" do
-      @users = [{ id: '2', first_name: 'firstname', last_name: 'last_name' },{ id: '3', first_name: 'firstname', last_name: 'last_name' }]
-      post :user_count, params: { users: @users }
-      expect(response.body).to eq('["firstname"]')
-end
+	let(:user_params) [{ id: '2', first_name: 'firstname', last_name: 'last_name' },{ id: '3', first_name: 'firstname', last_name: 'last_name' }]
+	
+	it "should send json response of first name and its count" do
+	  post :user_count, params: { users: user_params }
+      expect(response.body).to eq('["firstname"]') #incompelete results right now changw it to {'first_name' => 1}
 	end	
-
 end
